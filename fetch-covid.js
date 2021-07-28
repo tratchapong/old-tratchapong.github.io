13eaefa71c04a4711998268f921afb975ed1dc3b
tratchapong <tratchapong@gmail.com>
2021-07-28 10:56:39 +0700

    update api server + dashboard

--- fetch-covid.js
+++ fetch-covid.js
@@ -1,10 +1,17 @@
 
-const url = 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json'
+// const url = 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json'
+const url = 'https://static.easysunday.com/covid-19/getTodayCases.json'
 let data
 
-fetch(url).then( resp => resp.json() ).then( d => {
-  data=d 
-  for(let key in d)
-  if(document.getElementById(key) !== null)
-    document.getElementById(key).innerHTML += ` <b>${d[key]}</b>`
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  fetch(url).then( resp => resp.json() ).then( d => {
+    data=d 
+    for(let key in d) {
+    console.log(`${key} : ${d[key]}`)
+    if(document.getElementById(key) !== null)
+      document.getElementById(key).innerHTML += ` <b>${d[key]}</b>`
+    }
+  })
+  
 })
